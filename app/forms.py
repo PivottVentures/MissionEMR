4a61ededec25e9474de26438e9dc34384a44de73
wesgbrooks <wesgbrooks@gmail.com>
2014-06-03 17:00:41 -0400

    Patient form generated. Working to store form data in the database.

--- app/forms.py
+++ app/forms.py
@@ -2,7 +2,7 @@ from flask_wtf import Form
 from wtforms import StringField, SubmitField, IntegerField, RadioField, PasswordField
 from wtforms.validators import Required, Length
 
-class Patient(Form):
+class Patients(Form):
 	first_name = StringField('Prenom/First Name', validators=[Required()])
 	last_name = StringField('Nom/Last Name', validators=[Required()])
 	sex = RadioField('Sex/Gender', choices=[(0,'Male/Male'), (1,'Femelle/Female')])
